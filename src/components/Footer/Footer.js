1a683b7a2ab353802c887cbd1040c26d7c7e599c
Shannan Galbraith <s.galbraith08@gmail.com>
2022-09-17 19:30:35 +1000

    added basic footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,11 +7,15 @@ const Footer = () => {
     <div className="footer">
       <div className="footer_left">
         <Typography variant="overline">
-          Version: {process.env.REACT_APP_VERSION}
+          <p>Build version: {process.env.REACT_APP_VERSION}</p>
+          <p>Form Builder (POC)</p>
         </Typography>
       </div>
       <div className="footer_right">
-        <Typography variant="overline">
+        <Typography
+          variant="overline"
+          style={{ position: "absolute", bottom: "10px", right: "20px" }}
+        >
           &copy; {new Date().getFullYear()}
         </Typography>
       </div>
